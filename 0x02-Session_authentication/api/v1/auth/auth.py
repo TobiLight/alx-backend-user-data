ed70d618ff416625f05b2579c5d67e6d8cc9fb21
TobiLight <jgetitdonefast@gmail.com>
2024-02-15 23:56:38 +0100

    ✔ feat: Added new endpoint: GET /users/me to retrieve the authenticated User object

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -16,10 +16,10 @@ class Auth:
         if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        # path = path.rstrip("/")
+        path = path.rstrip("/")
 
         for excluded_path in excluded_paths:
-            # excluded_path = excluded_path.rstrip("/")
+            excluded_path = excluded_path.rstrip("/")
             if excluded_path.endswith("*"):
                 if path.startswith(excluded_path[:-1]):
                     return False
