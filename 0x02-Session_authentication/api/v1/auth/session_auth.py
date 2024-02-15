b1fabfb60176fe538c14d79bd308a7ea32cbe980
TobiLight <jgetitdonefast@gmail.com>
2024-02-16 00:06:26 +0100

    ✔ feat: Method that Creates and returns a Session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,4 +9,18 @@ class SessionAuth(Auth):
     """
     Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates and returns a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        from uuid import uuid4
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
