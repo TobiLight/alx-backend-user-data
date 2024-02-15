53f041c4a7ff37cc7e61d99ae5abde21e0e8aff6
TobiLight <jgetitdonefast@gmail.com>
2024-02-16 00:27:24 +0100

    ✔ feat: Updated before_request to exclude auth_session/login path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,13 +27,18 @@ def before_request():
     """
     Authenticates a user before processing requests.
     """
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
     if auth is None or not auth.\
-            require_auth(request.path,
-                         ['/api/v1/status/', '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']):
+            require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
