e18d09eb8eba93ad7c08f92a8725fbb11110c155
TobiLight <jgetitdonefast@gmail.com>
2024-02-15 23:28:12 +0100

    ✔ feat: Added new endpoint: GET /users/me to retrieve the authenticated User object

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,19 +27,20 @@ def before_request():
     """
     Authenticates a user before processing requests.
     """
-    if auth is None or not auth.\
-            require_auth(request.path,
-                         ['/api/v1/status/', '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
-
-    request.current_user = auth.current_user(request)
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 @app.errorhandler(404)
