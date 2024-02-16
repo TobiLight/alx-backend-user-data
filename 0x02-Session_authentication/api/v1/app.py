3211b7cc205d16c4d66b5ac55d6474b5d2124afa
TobiLight <jgetitdonefast@gmail.com>
2024-02-16 01:40:22 +0100

    ✔ feat: Method and route that logs a user out

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from flask import Flask, abort, jsonify, request
 from flask_cors import (CORS)
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-auth = SessionAuth()
 
 
 
@@ -23,7 +22,7 @@ if getenv('AUTH_TYPE') is not None:
         auth = BasicAuth()
     elif getenv('AUTH_TYPE') == 'session_auth':
         # from api.v1.auth.session_auth import SessionAuth
-        # auth = SessionAuth()
+        auth = SessionAuth()
     else:
         from api.v1.auth.auth import Auth
         auth = Auth()
