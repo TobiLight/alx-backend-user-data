c3538566abc490748bde33483ac77fc417452e25
TobiLight <jgetitdonefast@gmail.com>
2024-02-16 01:56:28 +0100

    ✔ feat: Method and route that logs a user out

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,8 +28,10 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Returns a User ID based on a Session ID"""
-        if type(session_id) is str:
-            return self.user_id_by_session_id.get(session_id)
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return str(self.user_id_by_session_id.get(session_id))
 
     def current_user(self, request=None):
         """
