5eac237d0fafe2a833fdac1f037e509f0b57630c
TobiLight <jgetitdonefast@gmail.com>
2024-02-23 00:33:08 +0100

    ✔  Resets a user's password using a valid reset token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -167,6 +167,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(
-                user_id=user.id, hashed_password=hashed_password, reset_token=None)
+                user_id=user.id, hashed_password=hashed_password,
+                reset_token=None)
         except NoResultFound:
             raise ValueError
