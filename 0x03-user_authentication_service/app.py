b3ee0c653d9fed723a5d43ab2b7b9ea26a659e81
TobiLight <jgetitdonefast@gmail.com>
2024-02-22 15:45:22 +0100

    ✔ Registers a new user and returns a JSON response.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,7 +29,7 @@ def user():
     try:
         user = AUTH.register_user(email=email, password=password)
         return jsonify({"email": user.email, "message": "user created"}), 201
-    except:
+    except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
 
