daaa24d39e4e82c5066a985994ce6698db9dc4e8
TobiLight <jgetitdonefast@gmail.com>
2024-02-22 02:49:35 +0100

    ✔ Registers a new user and returns the created User object.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,7 +19,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """
         Registers a new user and returns the created User object.
 
