0b38c254c50c7237a8c67ea391e55bb4e10d3500
TobiLight <jgetitdonefast@gmail.com>
2024-02-22 22:11:01 +0100

    ✔ Route that Handles user login requests and creates sessions.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic Flask App"""
 
 from typing import Tuple
-from flask import Flask, Response, jsonify, request
+from flask import Flask, Response, abort, jsonify, request
 from auth import Auth
 
 
@@ -31,6 +31,26 @@ def user():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Handles user login requests and creates sessions."""
+    form = request.form
+    email: str = form['email']
+    password: str = form['password']
+
+    session_id = None
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id,
+                            httponly=True, secure=True)
+
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
