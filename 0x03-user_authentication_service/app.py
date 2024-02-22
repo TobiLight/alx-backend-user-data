fa698b0e52556dcebbee0e9c41e45e30cf4e9aa7
TobiLight <jgetitdonefast@gmail.com>
2024-02-23 00:47:57 +0100

    ✔  Route that Handles password reset requests and updates passwords.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -102,19 +102,19 @@ def get_reset_password_token() -> Response:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> Response:
     """Handles password reset requests and updates passwords."""
+    form = request.form
+    email: str = form['email']
+    reset_token: str = form['reset_token']
+    new_password: str = form['new_password']
 
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        is_password_changed = True
     except ValueError:
-        is_password_changed = False
-    if not is_password_changed:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+
+    response = jsonify({"email": email, "message": "Password updated"})
+    response.status_code = 200
+    return response
 
 
 if __name__ == "__main__":
