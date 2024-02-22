55bd1ff8e96c5119e99980574fa114221855a56e
TobiLight <jgetitdonefast@gmail.com>
2024-02-22 23:17:59 +0100

    ✔ Invalidates the session for the given user.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -116,3 +116,16 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Invalidates the session for the given user.
+
+        Args:
+            user_id (int): The ID of the user whose session to invalidate.
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except Exception:
+            return None
