c79e3c7ccdeff616c5968319ec841bcb45593f1d
TobiLight <jgetitdonefast@gmail.com>
2024-02-22 02:27:07 +0100

    ✔ Updates a user's attributes and commits changes to the database.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """DB module
 """
-from xml.dom import NotFoundErr
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
