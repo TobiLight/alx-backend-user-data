a16f08b4733bc2429d7205c3c18f3960a95d318a
TobiLight <jgetitdonefast@gmail.com>
2024-02-23 01:16:38 +0100

    ✔ A simple end-to-end (E2E) integration test for .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,8 +43,8 @@ def login():
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie('session_id', session_id,
-                            httponly=True, secure=True)
+        response.status_code = 200
+        response.set_cookie('session_id', session_id)
 
         return response
     else:
